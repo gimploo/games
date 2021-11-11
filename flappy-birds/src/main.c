68a9ee81d3d3400d30b666d0d50c8f6b6c2114dc
gokulkrishnancm <GokulKrishnan949@gmail.com>
2021-11-11 17:39:32 +0530

    Update main.c

--- flappy-birds/src/main.c
+++ flappy-birds/src/main.c
@@ -1,42 +1,83 @@
 #include "../lib/simple/gl_renderer2d.h"
 #include "../lib/application.h"
+#include "../lib/game/2D/collision.h"
 
-window_t win;
-application_t game;
-gl_renderer2d_t renderer;
+typedef enum {
 
-void init(void *arg)
+    GAME_MENU,
+    GAME_START,
+    GAME_PAUSE,
+    GAME_PLAYING,
+    GAME_WON,
+    GAME_LOST,
+    GAME_OVER,
+    COUNT
+
+} game_state_type;
+
+typedef struct bird_t {
+
+    vec2f_t          position;
+    f32              speed;
+
+    boxcollider2d_t collider;
+
+    quadf_t         __vertices;
+    quadf_t         __texcoord;
+
+} bird_t;
+
+typedef struct game_t {
+
+    bird_t          player;
+    game_state_type state;
+
+} game_t;
+
+void init(application_t *app )
 {
-    gl_shader_t shader = gl_shader_default_init();
-    renderer = gl_renderer2d_init(&shader, NULL);
 
-    gl_vertex_t vertices[] = {
-
-    };
-    gl_batch_t batch = gl_batch_init(vertices, gl_quad_t);
 }
 
-void update(void *arg)
+void update(application_t *app)
 {
-    f32 fps = application_get_fps(&game);
-    f32 dt = application_get_dt(&game);
+    f32 fps = application_get_fps(app);
+    f32 dt = application_get_dt(app);
 
 }
 
-void render(void *arg)
+void render(application_t *app)
 {
-    window_gl_render_begin(&win);
+    window_t *win = application_get_whandle(app);
 
-    gl_renderer2d_draw_from_batch(&renderer, &batch);
+    window_gl_render_begin(win);
 
-    window_gl_render_end(&win);
+
+    window_gl_render_end(win);
 }
 
 int main(void)
 {
-    win = window_init("Flappy Birds", 700, 800, SDL_INIT_VIDEO);
-    game = application_init(&win, init, update, render);
+    window_t win = window_init("Flappy Birds", 700, 800, SDL_INIT_VIDEO);
+    gl_shader_t shader = gl_shader_from_file_init("../res/shader.vs", "../res/shader.fs");
+    gl_texture2d_t texture = gl_texture2d_init("../res/flappy-birds-texture-atlas.png");
+    gl_renderer2d_t renderer = gl_renderer2d_init(&shader, &texture);
+
+    application_t app = application_init(&win, &renderer);
+
+    app = (application_t ){
+        .init = init,
+        .update = update,
+        .render = render
+    };
+
+    application_run(&app);
 
 
-    application_run(&game);
+// Cleanup
+    window_destroy(&win);
+    gl_renderer2d_destroy(&renderer);
+    gl_shader_destroy(&shader);
+    gl_texture2d_destroy(&texture);
+
 }
