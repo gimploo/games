43d5a5ffe99d75a25e8580758f97be3bf467d028
gimploo <41667550+gimploo@users.noreply.github.com>
2023-09-16 15:42:10 +0530

    Making a fps game in poglib

---
+++ fps/src/scenes/menu.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <poglib/poggen.h>
+
+
+void menu_init(scene_t *scene)
+{
+    printf("Running menu scene\n");
+}
+
+void menu_input(scene_t *scene, const action_t action)
+{
+}
+
+void menu_update(scene_t *scene)
+{
+}
+
+void menu_render(scene_t *scene)
+{
+}
+
+void menu_destroy(scene_t *scene)
+{
+}
